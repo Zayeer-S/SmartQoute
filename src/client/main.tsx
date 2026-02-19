01917122f187b8aed64fb384b18b579cd0d22130
Zayeer-S <zayeersultan@gmail.com>
2026-02-19 07:36:37 +0000

    feat(client): make theme context

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -7,22 +7,26 @@ import CustomerPage from './pages/customer/CustomerPage';
 import AdminPage from './pages/admin/AdminPage';
 import CreateTicketPage from './pages/customer/CreateTicketPage';
 import { CLIENT_ROUTES } from './constants/client.routes';
+import { ThemeProvider } from './context/theme';
+import './styles/globals.css';
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <AuthProvider>
-        <Routes>
-          <Route path="/">
-            <Route index element={<LoginPage />} />
-            <Route path={CLIENT_ROUTES.LOGIN} element={<LoginPage />} />
-            <Route path={CLIENT_ROUTES.CUSTOMER} element={<CustomerPage />} />
-            <Route path={CLIENT_ROUTES.ADMIN} element={<AdminPage />} />
-            <Route path={CLIENT_ROUTES.CUSTOMER_CREATE} element={<CreateTicketPage />} />
-          </Route>
-        </Routes>
-      </AuthProvider>
+      <ThemeProvider>
+        <AuthProvider>
+          <Routes>
+            <Route path="/">
+              <Route index element={<LoginPage />} />
+              <Route path={CLIENT_ROUTES.LOGIN} element={<LoginPage />} />
+              <Route path={CLIENT_ROUTES.CUSTOMER} element={<CustomerPage />} />
+              <Route path={CLIENT_ROUTES.ADMIN} element={<AdminPage />} />
+              <Route path={CLIENT_ROUTES.CUSTOMER_CREATE} element={<CreateTicketPage />} />
+            </Route>
+          </Routes>
+        </AuthProvider>
+      </ThemeProvider>
     </BrowserRouter>
   </StrictMode>
 );
