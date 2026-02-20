08339bd5d393cb82d1330d0b1c5adb1660770d74
Zayeer-S <zayeersultan@gmail.com>
2026-02-20 08:38:02 +0000

    feat(client): update client routes

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -15,7 +15,15 @@ import DashboardPage from './pages/customer/DashboardPage';
 import TicketsPage from './pages/customer/TicketsPage';
 import TicketDetailPage from './pages/customer/TicketDetailPage';
 import NewTicketPage from './pages/customer/NewTicketPage';
-import SettingsPage from './pages/customer/SettingsPage';
+import SettingsPage from './pages/customer/CustomerSettingsPage';
+import AdminLayout from './pages/admin/AdminLayout';
+import AdminTicketsPage from './pages/admin/AdminTicketsPage';
+import AdminTicketDetailPage from './pages/admin/AdminTicketDetailPage';
+import AdminQuotesPage from './pages/admin/AdminQuotesPage';
+import AdminQuoteDetailPage from './pages/admin/AdminQuoteDetailPage';
+import AdminAnalyticsPage from './pages/admin/AdminAnalyticsPage';
+import AdminSLAPoliciesPage from './pages/admin/AdminSLAPoliciesPage';
+import AdminSettingsPage from './pages/admin/AdminSettingsPage';
 import './styles/globals.css';
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
@@ -41,7 +49,24 @@ createRoot(document.getElementById('root')!).render(
                       ]}
                     />
                   }
-                ></Route>
+                >
+                  <Route path={CLIENT_ROUTES.ADMIN.ROOT} element={<AdminLayout />}>
+                    <Route index element={<AdminTicketsPage />} />
+                    <Route path={CLIENT_ROUTES.ADMIN.TICKETS} element={<AdminTicketsPage />} />
+                    <Route
+                      path={CLIENT_ROUTES.ADMIN.TICKET()}
+                      element={<AdminTicketDetailPage />}
+                    />
+                    <Route path={CLIENT_ROUTES.ADMIN.QUOTES} element={<AdminQuotesPage />} />
+                    <Route path={CLIENT_ROUTES.ADMIN.QUOTE()} element={<AdminQuoteDetailPage />} />
+                    <Route path={CLIENT_ROUTES.ADMIN.ANALYTICS} element={<AdminAnalyticsPage />} />
+                    <Route
+                      path={CLIENT_ROUTES.ADMIN.SLA_POLICIES}
+                      element={<AdminSLAPoliciesPage />}
+                    />
+                    <Route path={CLIENT_ROUTES.ADMIN.SETTINGS} element={<AdminSettingsPage />} />
+                  </Route>
+                </Route>
 
                 <Route element={<ProtectedRoute allowedRoles={[AUTH_ROLES.CUSTOMER]} />}>
                   <Route path={CLIENT_ROUTES.CUSTOMER.ROOT} element={<CustomerLayout />}>
